4fc1ebff5685d74c6ba4a9f2e5209cdf65f884cf
geraldofigueiredo <geraldo.figueiredosj@gmail.com>
2021-08-19 00:31:28 -0300

    add tests to GetValueAt

--- data_structures/arrays/array_test.go
+++ data_structures/arrays/array_test.go
@@ -17,6 +17,7 @@ func TestNewVector(t *testing.T) {
 	}{
 		{"invalid capacity", args{capacity: 0}, vector{}, true},
 		{"min capacity", args{capacity: 1}, vector{capacity: 16, size: 0, data: make([]int, 16)}, false},
+		// {"capacity multiplied by growth rate", args{capacity: 20}, vector{capacity: 32, size: 0, data: make([]int, 32)}, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -27,6 +28,53 @@ func TestNewVector(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("NewVector() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_vector_GetValueAt(t *testing.T) {
+	defaultVector := vector{
+		size:     10,
+		capacity: 16,
+		data:     []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+	}
+
+	type fields struct {
+		size     int
+		capacity int
+		data     []int
+	}
+	type args struct {
+		index int
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    int
+		wantErr bool
+	}{
+		{name: "negative index", fields: fields(defaultVector), args: args{-1}, want: 0, wantErr: true},
+		{name: "index larger than size", fields: fields(defaultVector), args: args{defaultVector.size}, want: 0, wantErr: true},
+		{name: "valid index", fields: fields(defaultVector), args: args{5}, want: 5, wantErr: false},
+		{name: "first index", fields: fields(defaultVector), args: args{0}, want: 0, wantErr: false},
+		{name: "last index", fields: fields(defaultVector), args: args{defaultVector.size - 1}, want: 9, wantErr: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			v := vector{
+				size:     tt.fields.size,
+				capacity: tt.fields.capacity,
+				data:     tt.fields.data,
+			}
+			got, err := v.GetValueAt(tt.args.index)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("vector.GetValueAt() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("vector.GetValueAt() = %v, want %v", got, tt.want)
 			}
 		})
 	}
